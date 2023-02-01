7069c90fa021ccd8cdc5d1581e6774b54d2ab2f3
ivan.ogorodnyk <ogorodnyki@gmail.com>
2023-02-01 15:18:57 +0100

    Playing

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+#data = set("hello")
+data = {5, 7, 4, 8, 3, 5}
 
-user1 = input ("enter your number:")
-print ("My name is " + user1)
+data.add(21)
+data.update([123, True, 66, 2])
+data.remove(True)
+data.pop()
+#data.clear()
+
+nums = [5, 7, 3, 5, 5]
+new_nums = set(nums)
+
+new_data = frozenset([123, True, 66, 2, 5, 7, 3, 5, 5])
+
+
+print(new_data)
