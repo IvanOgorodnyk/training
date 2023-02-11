b66416632a51b06ee37e6de8bad964ffcb2128d6
IvanO <ogorodnyki@gmail.com>
2023-02-11 18:10:56 +0200

    class and object

--- lessons.py
+++ lessons.py
@@ -1,21 +1,56 @@
-class Cat:
-    name = None
-    age = None
-    isHappy = None
+class Building:
+    year = None
+    city = None
 
-    def __init__(self, name=None, age=None, isHappy=None):
-        self.set_data(name, age, isHappy)
-        self.get_data()
+    def __init__(self, year, city):
+        self.year = year
+        self.city = city
 
-    def set_data(self, name = None, age = None, isHappy = None):
-        self.name = name
-        self.age = age
-        self.isHappy = isHappy
-
-    def get_data(self):
-        print(self.name, "age:", self.age, ". Happy", self.isHappy)
+    def get_info(self):
+        print("Year:", self.year, ". City", self.city)
 
 
-cat1 = Cat()
+class School(Building):
+    pupils = 0
 
-cat2 = Cat("Shopa", 2, False)
+    def __init__(self, pupils,  year, city):
+        super(School, self).__init__(year, city)
+        self.pupils = pupils
+
+    def get_info(self):
+        super().get_info()
+        print("Pupils:", self.pupils)
+
+
+class House(Building):
+    residentors = 0
+
+    def __init__(self, residentors, year, city):
+        super(House, self).__init__(year, city)
+        self.residentors = residentors
+
+    def get_info(self):
+            super().get_info()
+            print("Residentors:", self.residentors)
+
+
+
+class Shop(Building):
+    visitors = 0
+
+    def __init__(self, visitors, year, city):
+        super(Shop, self).__init__(year, city)
+        self.visitors = visitors
+
+    def get_info(self):
+            super().get_info()
+            print("Visitors:", self.visitors)
+
+
+
+school = School(100, 2000, "Kyiv")
+school.get_info()
+house = House(88, 2000, "Kyiv")
+house.get_info()
+shop = Shop(200, 2000, "Kyiv")
+shop.get_info()
