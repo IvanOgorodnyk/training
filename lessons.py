94cfbe0e655f0e8e845d6f0f9f0278a00daa2c19
IvanO <ogorodnyki@gmail.com>
2023-02-11 19:10:22 +0200

    class and object

--- lessons.py
+++ lessons.py
@@ -1,56 +1,19 @@
-class Building:
-    year = None
-    city = None
-
-    def __init__(self, year, city):
-        self.year = year
-        self.city = city
-
-    def get_info(self):
-        print("Year:", self.year, ". City", self.city)
+import webbrowser
 
 
-class School(Building):
-    pupils = 0
-
-    def __init__(self, pupils,  year, city):
-        super(School, self).__init__(year, city)
-        self.pupils = pupils
-
-    def get_info(self):
-        super().get_info()
-        print("Pupils:", self.pupils)
+def validator(func):
+    def wrapper(url):
+        if "." in url:
+            func(url)
+        else:
+            print("incorrect url")
+    return wrapper
 
 
-class House(Building):
-    residentors = 0
-
-    def __init__(self, residentors, year, city):
-        super(House, self).__init__(year, city)
-        self.residentors = residentors
-
-    def get_info(self):
-            super().get_info()
-            print("Residentors:", self.residentors)
+@validator
+def open_url(url):
+    webbrowser.open(url)
 
 
 
-class Shop(Building):
-    visitors = 0
-
-    def __init__(self, visitors, year, city):
-        super(Shop, self).__init__(year, city)
-        self.visitors = visitors
-
-    def get_info(self):
-            super().get_info()
-            print("Visitors:", self.visitors)
-
-
-
-school = School(100, 2000, "Kyiv")
-school.get_info()
-house = House(88, 2000, "Kyiv")
-house.get_info()
-shop = Shop(200, 2000, "Kyiv")
-shop.get_info()
+open_url("https://github.com")
