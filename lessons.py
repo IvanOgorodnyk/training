ad1390000b08fc47ac0cc18d228b5b813648bbcd
IvanO <ogorodnyki@gmail.com>
2023-02-11 17:09:05 +0200

    class and object

--- lessons.py
+++ lessons.py
@@ -1,10 +1,22 @@
-data = (4, 6, 7, 3, 5, True, 4.3, "Yes")
+class Cat:
+    name = None
+    age = None
+    isHappy = None
 
-for el in data:
-    print(el)
+    def set_data(self, name, age, isHappy):
+        self.name = name
+        self.age = age
+        self.isHappy = isHappy
 
-nums = [5, 7, 8]
+    def get_data(self):
+        print(self.name, "age:", self.age, ". Happy", self.isHappy)
 
-new_data = tuple(nums)
-word = tuple("Hello world")
-print(word)
+
+cat1 = Cat()
+cat1.set_data("Barsik", 3, True)
+
+cat2 = Cat()
+cat2.set_data("Shopa", 2, False)
+
+cat1.get_data()
+cat2.get_data()
--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-n = int(input("enter lenth: "))
-
-user_list = []
-
-i = 0
-while i < n:
-    string = "enter element #" + str(i + 14) + ":"
-    user_list.append(input(string))
-    i += 1
-
-print(user_list)
+import cowsay
+cowsay.cow("hello world")
